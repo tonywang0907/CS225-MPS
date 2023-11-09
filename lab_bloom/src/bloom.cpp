f6d31413067f159e381622180cb7ab4833cff4b8
Brad Solomon <bradsol@Brads-MacBook-Air-2.local>
2023-11-08 21:30:41 -0600

    release lab_bloom

---
+++ lab_bloom/src/bloom.cpp
@@ -0,0 +1,62 @@
+/**
+ * @file bloom.cpp
+ * Definitions of the binary tree functions you'll be writing for this lab.
+ * You'll need to modify this file.
+ */
+
+ #include "bloom.h"
+
+BF::BF(uint64_t size, std::vector<hashFunction> hashList){
+
+    // Your code here 
+}
+
+BF::BF(const BF& other){
+    // Your code here 
+}
+
+BF::~BF(){
+    return;
+}
+
+void BF::add(const int & key){
+
+    // Your code here 
+}
+
+bool BF::contains(const int& key) const{
+    // Your code here 
+
+    return false;
+}
+
+void BF::bit_union(const BF& other){
+    // Your code here 
+}
+
+void BF::intersect(const BF& other){
+    // Your code here 
+}
+
+float measureFPR(std::vector<int> inList, uint64_t size, std::vector<hashFunction> hashList, unsigned max){
+    // Your code here
+
+    return 9999.999;
+
+}
+
+bool getBitFromArray(std::vector<char> bv, int index){
+    // Your code here
+
+
+    return 'a';
+}
+
+bool getBitFromByte(char in, int index){
+    // Your code here
+
+
+    return true;
+
+}
+
